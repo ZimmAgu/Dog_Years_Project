139030ce77f9defdf301dbf0abb876eb7ca2ce62
Zim Agu <zimmagu@gmail.com>
2021-01-03 17:18:33 -0800

    Create Dog_Years.js
    Finished the project

---
+++ Dog_Years.js
@@ -0,0 +1,43 @@
+
+//I am currently 20 years old 
+const MYAGE = 20;
+
+
+
+
+
+
+//The earlyYears variable is assigned value 2
+let earlyYears = 2;
+
+// Multiplyin th value saved to early years by 10.5
+earlyYears *= 10.5;
+
+
+
+
+// Setting the laterYears variable to 2 less than my age
+let laterYears = MYAGE - 2;
+
+// calculates the number of dog years accounted for by my later years.
+laterYears *= 4;
+
+
+
+
+
+
+/*  My age in dog years is 
+    early years and later years combined */
+ let myAgeinDogYears = earlyYears + laterYears;
+
+
+
+ // Telling the program my name
+ myName = `Zim Agu`.toLowerCase();
+
+
+
+
+ console.log(`My name is ${myName}. I am ${MYAGE} years old in human years which is ${myAgeinDogYears} years old in dog years.`);
+
